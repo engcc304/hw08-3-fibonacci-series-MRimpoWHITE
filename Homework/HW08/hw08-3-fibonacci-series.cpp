f9defb29c1e54ddd546aa034069df8d5b4e14922
MRimpoWHITE <phichitphu2015@gmail.com>
2023-08-26 19:10:25 +0700

    uPUpUppp

--- Homework/HW08/hw08-3-fibonacci-series.cpp
+++ Homework/HW08/hw08-3-fibonacci-series.cpp
@@ -8,3 +8,29 @@
         Sum = 54
 
 */
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    int a = 1, b = 1; // ค่าเริ่มต้นสองค่าแรกในอนุกรม
+    int sum = a + b; // ผลรวมเริ่มต้นเป็นผลรวมของสองค่าแรก
+
+    printf("Series = %d + %d", a, b);
+
+    for (int i = 3; i <= n; ++i) {
+        int c = a + b; // คำนวณค่าต่อไปในอนุกรม
+        sum += c; // เพิ่มค่า c เข้าไปในผลรวม
+        printf(" + %d", c);
+
+        a = b;
+        b = c;
+    }
+
+    printf("\nSum = %d\n", sum);
+
+    return 0;
+}
